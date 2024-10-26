7dbdc083e5526fbcbaa8b8d646797838b34c7ab4
Vishal Kumar <vsnappy1@gmail.com>
2024-10-26 13:43:47 -0700

    Added support for drawables.

--- resourcemanager/src/main/java/com/randos/resourcemanager/file/generation/ClassFileGenerator.kt
+++ resourcemanager/src/main/java/com/randos/resourcemanager/file/generation/ClassFileGenerator.kt
@@ -3,98 +3,132 @@ package com.randos.resourcemanager.file.generation
 import com.randos.resourcemanager.file.parser.XmlParser
 import com.randos.resourcemanager.model.Resource
 import com.randos.resourcemanager.model.ResourceType
+import com.randos.resourcemanager.model.ValueResource
+import com.randos.resourcemanager.model.ValueResourceType
 import com.randos.resourcemanager.utils.toCamelCase
 import java.io.File
 
 internal class ClassFileGenerator {
 
     companion object {
-        fun generateClassFile(packageName: String, files: List<File>): String {
+        fun generateClassFile(packageName: String, files: List<Resource>): String {
             return StringBuilder().apply {
                 appendLine("package $packageName\n")
                 appendLine("import android.app.Application")
+                appendLine("import android.graphics.drawable.Drawable")
                 appendLine("import ${packageName}.R\n")
                 appendLine("object ResourceManager {\n")
                 appendLine("\tprivate var _application: Application? = null")
                 appendLine("\tprivate val application: Application")
-                appendLine("\t\tget() = _application ?: throw IllegalStateException(\"Application is not initialized, seems like you forgot to invoke ResourceManager.initialize(this) in application class.\")\n")
+                appendLine("\t\tget() = _application ?: throw IllegalStateException(\"ResourceManager is not initialized. Please call ResourceManager.initialize(this) in your Application class.\")\n")
                 appendLine("\tfun initialize(application: Application) {")
                 appendLine("\t\t_application = application")
                 appendLine("\t}\n")
 
-                files.forEach { file ->
-                    val resources = XmlParser.parseXML(file)
-                    // Only create object for file when given file has some resources (i.e. not empty).
-                    if (resources.isNotEmpty()) {
-                        val resourceObject = generateObject(
-                            file.nameWithoutExtension,
-                            resources
-                        )
-                        appendLine(resourceObject)
+                files.forEach { resource ->
+                    appendLine("\t// ----- ${resource.type::class.simpleName} -----")
+                    when (resource.type) {
+                        ResourceType.VALUES -> {
+                            generateObjectForValueResources(resource)
+                        }
+
+                        ResourceType.DRAWABLES -> {
+                            generateObjectForDrawableResources(resource)
+                        }
                     }
                 }
                 appendLine("}")
             }.toString()
         }
 
-        private fun generateObject(name: String, pairs: List<Resource>): String {
+        private fun StringBuilder.generateObjectForDrawableResources(resource: Resource) {
+            appendLine("\tobject Drawables {")
+            resource.directoryPath.listFiles()?.forEach { file ->
+                appendDrawableResource(file.nameWithoutExtension, "\t\t")
+            }
+            appendLine("\t}")
+        }
+
+        private fun StringBuilder.generateObjectForValueResources(resource: Resource) {
+            resource.directoryPath.listFiles().getXmlFiles().forEach { file ->
+                val resources = XmlParser.parseXML(file)
+                // Only create object for file when given file has some resources (i.e. not empty).
+                if (resources.isNotEmpty()) {
+                    val resourceObject = generateObject(
+                        file.nameWithoutExtension,
+                        resources
+                    )
+                    appendLine(resourceObject)
+                }
+            }
+        }
+
+        private fun generateObject(name: String, pairs: List<ValueResource>): String {
             val defaultIndentation = "\t\t"
             return StringBuilder().apply {
                 appendLine("\tobject ${name.toCamelCase().replaceFirstChar { it.uppercase() }} {")
                 pairs.forEach { resource ->
                     when (resource.type) {
-                        ResourceType.Array -> appendStringArrayResource(resource, defaultIndentation)
-                        ResourceType.Boolean -> appendBooleanResource(resource, defaultIndentation)
-                        ResourceType.Color -> appendColorResource(resource, defaultIndentation)
-                        ResourceType.Dimension -> appendDimensionResource(resource, defaultIndentation)
-                        ResourceType.Fraction -> appendFractionResource(resource, defaultIndentation)
-                        ResourceType.IntArray -> appendIntArrayResource(resource, defaultIndentation)
-                        ResourceType.Integer -> appendIntegerResource(resource, defaultIndentation)
-                        ResourceType.Plural -> appendPluralResource(resource, defaultIndentation)
-                        ResourceType.String -> appendStringResource(resource, defaultIndentation)
-                        ResourceType.StringArray -> appendStringArrayResource(resource, defaultIndentation)
+                        ValueResourceType.Array -> appendStringArrayResource(resource, defaultIndentation)
+                        ValueResourceType.Boolean -> appendBooleanResource(resource, defaultIndentation)
+                        ValueResourceType.Color -> appendColorResource(resource, defaultIndentation)
+                        ValueResourceType.Dimension -> appendDimensionResource(resource, defaultIndentation)
+                        ValueResourceType.Fraction -> appendFractionResource(resource, defaultIndentation)
+                        ValueResourceType.IntArray -> appendIntArrayResource(resource, defaultIndentation)
+                        ValueResourceType.Integer -> appendIntegerResource(resource, defaultIndentation)
+                        ValueResourceType.Plural -> appendPluralResource(resource, defaultIndentation)
+                        ValueResourceType.String -> appendStringResource(resource, defaultIndentation)
+                        ValueResourceType.StringArray -> appendStringArrayResource(resource, defaultIndentation)
                     }
                 }
                 appendLine("\t}")
             }.toString()
         }
 
-        private fun StringBuilder.appendDimensionResource(resource: Resource, defaultIndentation: String) {
+        private fun StringBuilder.appendDrawableResource(name: String, defaultIndentation: String) {
+            appendLine("${defaultIndentation}fun ${name.toCamelCase()}() : Drawable = application.resources.getDrawable(R.drawable.${name}, application.theme)")
+        }
+
+        private fun StringBuilder.appendDimensionResource(resource: ValueResource, defaultIndentation: String) {
             appendLine("${defaultIndentation}fun ${resource.name.toCamelCase()}() : Float = application.resources.getDimension(R.dimen.${resource.name})")
         }
 
-        private fun StringBuilder.appendColorResource(resource: Resource, defaultIndentation: String) {
+        private fun StringBuilder.appendColorResource(resource: ValueResource, defaultIndentation: String) {
             appendLine("${defaultIndentation}fun ${resource.name.toCamelCase()}() : Int = application.resources.getColor(R.color.${resource.name}, application.theme)")
         }
 
-        private fun StringBuilder.appendIntegerResource(resource: Resource, defaultIndentation: String) {
+        private fun StringBuilder.appendIntegerResource(resource: ValueResource, defaultIndentation: String) {
             appendLine("${defaultIndentation}fun ${resource.name.toCamelCase()}() : Int = application.resources.getInteger(R.integer.${resource.name})")
         }
 
-        private fun StringBuilder.appendBooleanResource(resource: Resource, defaultIndentation: String) {
+        private fun StringBuilder.appendBooleanResource(resource: ValueResource, defaultIndentation: String) {
             appendLine("${defaultIndentation}fun ${resource.name.toCamelCase()}() : Boolean = application.resources.getBoolean(R.bool.${resource.name})")
         }
 
-        private fun StringBuilder.appendFractionResource(resource: Resource, defaultIndentation: String) {
+        private fun StringBuilder.appendFractionResource(resource: ValueResource, defaultIndentation: String) {
             appendLine("${defaultIndentation}fun ${resource.name.toCamelCase()}() : Float = application.resources.getFraction(R.fraction.${resource.name}, 0, 0)")
         }
 
-        private fun StringBuilder.appendStringResource(resource: Resource, defaultIndentation: String) {
+        private fun StringBuilder.appendStringResource(resource: ValueResource, defaultIndentation: String) {
             appendLine("${defaultIndentation}fun ${resource.name.toCamelCase()}() : String = application.resources.getString(R.string.${resource.name})")
         }
 
-        private fun StringBuilder.appendPluralResource(resource: Resource, defaultIndentation: String) {
+        private fun StringBuilder.appendPluralResource(resource: ValueResource, defaultIndentation: String) {
             appendLine("${defaultIndentation}fun ${resource.name.toCamelCase()}(quantity: Int) : String = application.resources.getQuantityString(R.plurals.${resource.name}, quantity)")
         }
 
-        private fun StringBuilder.appendStringArrayResource(resource: Resource, defaultIndentation: String) {
+        private fun StringBuilder.appendStringArrayResource(resource: ValueResource, defaultIndentation: String) {
             appendLine("${defaultIndentation}fun ${resource.name.toCamelCase()}() : kotlin.Array<String> = application.resources.getStringArray(R.array.${resource.name})")
         }
 
-        private fun StringBuilder.appendIntArrayResource(resource: Resource, defaultIndentation: String) {
+        private fun StringBuilder.appendIntArrayResource(resource: ValueResource, defaultIndentation: String) {
             appendLine("${defaultIndentation}fun ${resource.name.toCamelCase()}() : IntArray = application.resources.getIntArray(R.array.${resource.name})")
         }
     }
+}
+
+private fun Array<File>?.getXmlFiles(): List<File> {
+    return this?.filter { it.extension == "xml" } ?: emptyList()
 }
 
 
--- resourcemanager/src/main/java/com/randos/resourcemanager/model/Resource.kt
+++ resourcemanager/src/main/java/com/randos/resourcemanager/model/Resource.kt
@@ -1,6 +1,15 @@
 package com.randos.resourcemanager.model
 
-internal data class Resource(
-    val name: String,
+import java.io.File
+
+/**
+ * Represents a resource in the application, including its type and file path.
+ *
+ * @property type refers to type of Resource.
+ * @property directoryPath path to the directory of resource.
+ * @see [ResourceType]
+ */
+data class Resource(
     val type: ResourceType,
+    val directoryPath: File
 )
