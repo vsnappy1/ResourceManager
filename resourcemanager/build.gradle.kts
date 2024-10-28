1081e9186d53fed8ae3080a3dbbe383112270652
Vishal Kumar <vsnappy1@gmail.com>
2024-10-28 07:44:20 -0700

    Prepared for maven central publication, added build tasks.

--- resourcemanager/build.gradle.kts
+++ resourcemanager/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
@@ -20,7 +22,9 @@ plugins {
  * }
  * ```
  */
-val kspVersion: String = if(project.findProperty("kspVersion") != null) project.findProperty("kspVersion").toString() else "1.9.0-1.0.12"
+val kspVersion: String =
+    if (project.findProperty("kspVersion") != null) project.findProperty("kspVersion")
+        .toString() else "1.9.0-1.0.12"
 
 dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:$kspVersion")
@@ -35,14 +39,92 @@ kotlin {
     jvmToolchain(11)
 }
 
+// Task to generate Javadoc JAR
+tasks.register<Jar>("generateJavadocJar") {
+    group = "build"
+    description = "Generates a JAR file containing Javadoc."
+    dependsOn(tasks.named("javadoc"))
+    val publication = (publishing.publications.getByName("java") as MavenPublication)
+    archiveBaseName.set(publication.artifactId)
+    archiveVersion.set(publication.version)
+    archiveClassifier.set("javadoc")
+    from(provider { tasks.named<Javadoc>("javadoc").get().destinationDir })
+}
+
+// Task to generate sources JAR
+tasks.register<Jar>("generateSourcesJar") {
+    group = "build"
+    description = "Generates a JAR file containing source files."
+    val publication = (publishing.publications.getByName("java") as MavenPublication)
+    archiveBaseName.set(publication.artifactId)
+    archiveVersion.set(publication.version)
+    archiveClassifier.set("sources")
+    from(sourceSets["main"].allSource)
+}
+
+// Task to generate the POM file
+tasks.register<GenerateMavenPom>("generatePomXml") {
+    group = "build"
+    description = "Generates the POM XML file for the Maven publication."
+    val publication = (publishing.publications.getByName("java") as MavenPublication)
+    destination =
+        file(layout.buildDirectory.dir("generated/maven/${publication.artifactId}-${publication.version}.pom"))
+    pom = publication.pom
+}
+
+// Combined task to execute all tasks
+tasks.register("generateArtifacts") {
+    group = "build"
+    description = "Generates Javadoc JAR, sources JAR, and POM XML file."
+    dependsOn("build", "generateJavadocJar", "generateSourcesJar", "generatePomXml")
+    doLast {
+        println("All artifacts (Javadoc JAR, sources JAR, and POM) have been generated successfully.")
+    }
+}
+
+tasks.named("build") {
+    val publication = (publishing.publications.getByName("java") as MavenPublication)
+    archivesName = "${publication.artifactId}-${publication.version}"
+}
+
 afterEvaluate {
     publishing {
         publications {
-            create<MavenPublication>("java"){
+            create<MavenPublication>("java") {
                 from(components["java"])
                 groupId = "dev.randos"
                 artifactId = "resourcemanager"
                 version = "1.0.0"
+
+                pom {
+                    name = "${groupId}:${artifactId}"
+                    description =
+                        "An Android library that simplifies accessing Android resources (strings, colors, drawables, etc.) in both Android and non-Android components (e.g. ViewModel) using generated code."
+                    url = "https://github.com/vsnappy1/ResourceManager"
+
+                    licenses {
+                        license {
+                            name = "The Apache License, Version 2.0"
+                            url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            name = "Vishal Kumar"
+                            email = "vsnappy1@gmail.com"
+                            organization = "Randos"
+                            organizationUrl = "http://www.randos.dev"
+                        }
+                    }
+
+                    scm {
+                        connection = "scm:git:git://github.com/vsnappy1/ResourceManager.git"
+                        developerConnection =
+                            "scm:git:ssh://github.com/vsnappy1/ResourceManager.git"
+                        url = "https://github.com/vsnappy1/ResourceManager"
+                    }
+                }
             }
         }
     }
