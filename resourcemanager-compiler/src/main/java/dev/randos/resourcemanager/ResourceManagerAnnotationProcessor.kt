bf962652df41a537ec959d6c10a7fb0785b53440
Vishal Kumar <vsnappy1@gmail.com>
2024-11-03 20:44:58 -0800

    Added caching mechanism to optimize build time by reducing unnecessary file regenerations.

--- resourcemanager-compiler/src/main/java/dev/randos/resourcemanager/ResourceManagerAnnotationProcessor.kt
+++ resourcemanager-compiler/src/main/java/dev/randos/resourcemanager/ResourceManagerAnnotationProcessor.kt
@@ -7,6 +7,7 @@ import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import dev.randos.resourcemanager.file.generation.ClassFileGenerator
+import dev.randos.resourcemanager.manager.CacheManager
 import dev.randos.resourcemanager.model.Resource
 import dev.randos.resourcemanager.model.ResourceType
 import java.io.File
@@ -16,37 +17,74 @@ internal class ResourceManagerAnnotationProcessor(
 ) : SymbolProcessor {
     private val logger = environment.logger
     private val codeGenerator = environment.codeGenerator
-    private val annotationQualifiedName = "dev.randos.resourcemanager.runtime.InstallResourceManager"
+    private val annotationQualifiedName =
+        "dev.randos.resourcemanager.runtime.InstallResourceManager"
+    private lateinit var cacheManager: CacheManager
 
     override fun process(resolver: Resolver): List<KSAnnotated> {
         logger.info("Processing annotations")
 
+        // Get all symbols annotated with [InstallResourceManager] annotation.
         val symbols = resolver.getSymbolsWithAnnotation(annotationQualifiedName)
-        symbols.filterIsInstance<KSClassDeclaration>().forEach {
-            val containingFile = it.containingFile
-            if (containingFile != null) {
-                val className = it.simpleName.asString()
-                val packageName = it.packageName.asString()
+            .filterIsInstance<KSClassDeclaration>()
+
+        val unprocessedSymbols = mutableListOf<KSAnnotated>()
+
+        for (symbol in symbols) {
+            val containingFile = symbol.containingFile ?: continue
+            val annotatedFile = File(containingFile.filePath)
+            val resources = getResources(annotatedFile)
+            val packageName = symbol.packageName.asString()
+
+            cacheManager = CacheManager(
+                buildDirectory = annotatedFile.getPathToBuildDirectory(),
+                filesUnderObservation = resources.getFilesUnderObservation()
+            )
+
+            try {
+                // Create the new file with a dependency on `containingFile`
                 val file = codeGenerator.createNewFile(
-                    // TODO Pass in the list of files in Dependency so ksp can observe and when these files changes only then regenerate code otherwise not.
                     dependencies = Dependencies(true, containingFile),
                     packageName = packageName,
                     fileName = "ResourceManager"
                 )
 
+                // Write the generated class content to the file
                 file.bufferedWriter().use { out ->
-                    val resources = getResources(File(containingFile.filePath))
-                    val classFile = ClassFileGenerator.generateClassFile(
-                        packageName = packageName,
-                        namespace = getNameSpace(it) ?: packageName,
-                        files = resources
-                    )
+
+                    var classFile: String? = null
+                    // Check if the cache is up-to-date to avoid unnecessary regeneration
+                    if (cacheManager.isCacheUpToDate()) {
+                        // Get cached data directly if cache is valid
+                        classFile = cacheManager.getCachedContent()
+                    } else {
+                        // Invalidate outdated cache to prepare for fresh generation
+                        cacheManager.invalidateCache()
+                    }
+
+                    if (classFile == null) {
+                        classFile = ClassFileGenerator.generateClassFile(
+                            packageName = packageName,
+                            namespace = getNameSpace(symbol) ?: packageName,
+                            files = resources
+                        )
+                    }
+
                     out.write(classFile)
+                    out.close()
                 }
-                logger.info("Generated file for $className")
+                // Store the latest generated ResourceManager file in the cache for future use.
+                cacheManager.cache()
+                logger.info("Generated ResourceManager file for package $packageName.", symbol)
+            } catch (e: Exception) {
+                logger.error(
+                    "Error generating ResourceManager file for package $packageName: ${e.message}",
+                    symbol
+                )
+                unprocessedSymbols.add(symbol)
             }
         }
-        return emptyList()
+        return unprocessedSymbols
     }
 
     private fun getNameSpace(it: KSClassDeclaration): String? {
@@ -89,5 +127,38 @@ internal class ResourceManagerAnnotationProcessor(
             e.printStackTrace()
             emptyList()
         }
+    }
+
+    /**
+     * Generates a list of all files within the directories specified by each `Resource` object.
+     *
+     * @receiver List of `Resource` objects, each containing a directory path.
+     * @return A list of all files under each directory in the `Resource` list.
+     */
+    private fun List<Resource>.getFilesUnderObservation(): List<File> {
+        val files = mutableListOf<File>()
+        this.map { it.directoryPath }.forEach { directory ->
+            directory.walkTopDown().forEach { file ->
+                if (file.isFile) {
+                    files.add(file)
+                }
+            }
+        }
+        return files
+    }
+
+    /**
+     * Determines the `build` directory path for a given source file.
+     *
+     * @receiver The `File` object representing a file in the project.
+     * @return The path to the `build` directory if found; `null` otherwise.
+     */
+    private fun File.getPathToBuildDirectory(): File? {
+        var file = this
+        while (file.name != "src") {
+            file = file.parentFile ?: return null
+        }
+        val buildDirectory = File(file.parentFile, "build")
+        return if (buildDirectory.exists()) buildDirectory else null
     }
 }
---
+++ resourcemanager-compiler/src/main/java/dev/randos/resourcemanager/manager/CacheManager.kt
@@ -0,0 +1,111 @@
+package dev.randos.resourcemanager.manager
+
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+/**
+ * Manages caching operations for the generated resource files to reduce redundant file generation
+ * and support cache reuse when there are no relevant changes in observed files.
+ *
+ * @property buildDirectory The build directory where cache and generated files are located.
+ * @property filesUnderObservation A list of files whose last modified timestamps are observed for changes.
+ */
+class CacheManager(
+    private val buildDirectory: File?,
+    private val filesUnderObservation: List<File>
+) {
+
+    companion object {
+        private const val CACHE_DIRECTORY_PATH = "cache/dev/randos/resourcemanager"
+        private const val CONTENT_FILE_NAME = "content.txt"
+        private const val LAST_MODIFIED_TIMESTAMP_FILE_NAME = "last_modified_timestamp.txt"
+    }
+
+    /**
+     * Caches the content, path, and last modified timestamp of the generated ResourceManager file
+     * if it exists in the KSP-generated directory.
+     */
+    fun cache() {
+        val moduleName = buildDirectory?.parentFile?.name ?: return
+
+        val kspDirectory = File(buildDirectory, "generated/ksp")
+        if (!kspDirectory.exists()) return
+
+        val resourceManagerFile =
+            kspDirectory.walkTopDown().find { it.nameWithoutExtension == "ResourceManager" }
+                ?: return
+
+        val cacheDirectory = File(buildDirectory, "$CACHE_DIRECTORY_PATH/$moduleName")
+        cacheDirectory.mkdirs()
+
+        val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+        // Cache the content, file path, and last modified timestamp
+        File(cacheDirectory, CONTENT_FILE_NAME)
+            .writeText("${resourceManagerFile.readText()}// Cached on $timestamp")
+        File(cacheDirectory, LAST_MODIFIED_TIMESTAMP_FILE_NAME)
+            .writeText(getMostRecentLastModifiedTimestamp())
+
+        println("ResourceManager: Cached successfully.")
+    }
+
+    /**
+     * Retrieves the cached content of the ResourceManager file.
+     *
+     * @return The cached content as a String, or null if the cache does not exist.
+     */
+    fun getCachedContent() : String? {
+        val moduleName = buildDirectory?.parentFile?.name ?: return null
+        val cacheDirectory = File(buildDirectory, "$CACHE_DIRECTORY_PATH/$moduleName")
+
+        val contentFile = File(cacheDirectory, CONTENT_FILE_NAME)
+        if (contentFile.exists()) {
+            return contentFile.readText()
+        }
+        return null
+    }
+
+    /**
+     * Invalidates the cache by deleting the cached files for the module.
+     */
+    fun invalidateCache() {
+        val moduleName = buildDirectory?.parentFile?.name ?: return
+        val cacheDirectory = File(buildDirectory, "$CACHE_DIRECTORY_PATH/$moduleName")
+
+        if (cacheDirectory.exists()) {
+            if (cacheDirectory.deleteRecursively()) {
+                println("ResourceManager: Cache invalidated successfully.")
+            } else {
+                println("ResourceManager: Cache invalidation failed.")
+            }
+        }
+    }
+
+    /**
+     * Checks if the cache is up to date by comparing the stored last modified timestamps of observed files
+     * with the current timestamps.
+     *
+     * @return `true` if cache is up to date, `false` otherwise.
+     */
+    fun isCacheUpToDate(): Boolean {
+        val moduleName = buildDirectory?.parentFile?.name ?: return false
+        val cacheDirectory = File(buildDirectory, "$CACHE_DIRECTORY_PATH/$moduleName")
+
+        val lastModifiedTimestampFile = File(cacheDirectory, LAST_MODIFIED_TIMESTAMP_FILE_NAME)
+        if (lastModifiedTimestampFile.exists()) {
+            val storedTimestamp = getMostRecentLastModifiedTimestamp()
+            return storedTimestamp == lastModifiedTimestampFile.readText()
+        }
+        return false
+    }
+
+    /**
+     * Retrieves the most recent last modified timestamp from the list of  files under observation.
+     *
+     * @return The most recent last modified timestamp as a string.
+     */
+    private fun getMostRecentLastModifiedTimestamp(): String {
+        val mostRecentTimestamp = filesUnderObservation.maxOfOrNull { it.lastModified() } ?: 0L
+        return mostRecentTimestamp.toString()
+    }
+}
