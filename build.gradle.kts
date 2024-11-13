b5de1c50e9bb482693e7629fe25e519a6d611232
Vishal Kumar <vsnappy1@gmail.com>
2024-11-13 11:02:36 -0800

    Updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
-    id("dev.randos.resourcemanager") version "0.0.1" apply false
 }
 
 ext {
